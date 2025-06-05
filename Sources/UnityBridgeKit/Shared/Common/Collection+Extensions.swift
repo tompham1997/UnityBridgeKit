26ea937eeafb6c6250b3a6e8960bfbc80e3838af
TomFluentjoy <tom@fluentjoy.com>
2025-06-05 12:13:52 +0700

    🍀 update extensions scope

--- Sources/UnityBridgeKit/Shared/Common/Collection+Extensions.swift
+++ Sources/UnityBridgeKit/Shared/Common/Collection+Extensions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Collection extensions
-public extension Collection {
+extension Collection {
     
     /// Returns the element at the specified index if it is within bounds, otherwise nil.
     var isNotEmpty: Bool {
